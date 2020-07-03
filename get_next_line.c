b92cc3afe197d866d531cdf557626cf2285cf13e
Makhmud Ego <makhmudego@yandex.ru>
2020-07-03 17:30:31 +0300

    обработал все, кроме когда read возвращает 0;

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,13 @@
 /*   By: mizola <mizola@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 18:27:01 by mizola            #+#    #+#             */
-/*   Updated: 2020/07/03 15:23:17 by mizola           ###   ########.fr       */
+/*   Updated: 2020/07/03 17:24:57 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+
 
 int	get_next_line(int fd, char **line)
 {
@@ -18,33 +20,29 @@ int	get_next_line(int fd, char **line)
 	int buff;
 	int readed;
 	char *str_tmp;
-	int i;
 	int tmp;
 
-	i = 0;
 	tmp = 0;
 	buff = 	BUFFER_SIZE;
 	str_tmp = malloc(BUFFER_SIZE);
+	printf("\n%s\n", fds[fd]);
 	while ((readed = read(fd, str_tmp, buff)) > 0)
 	{
 		str_tmp[readed] = '\0';
-//		printf("\n%s\n", fds[fd]);
 		fds[fd] = ft_strjoin(fds[fd], str_tmp);
-		printf("\n%s\n", fds[fd]);
-//		free(str_tmp);//
 		while (fds[fd][tmp] != '\0')
 		{
 			if (fds[fd][tmp] == '\n')
 			{
-
 				*line = ft_substr(fds[fd], 0, tmp);
 				free(str_tmp);
+				str_tmp = ft_strdup(fds[fd] + tmp + 1);
+				free(fds[fd]);
+				fds[fd] = str_tmp;
 				return (1);
 			}
-//			fds[fd][tmp];
 			tmp++;
 		}
-//		str_tmp = malloc(buff += BUFFER_SIZE);
 	}
 	return 0;
 }
@@ -55,9 +53,14 @@ int	main()
 	char *line;
 
 	line = 0x0;
+	int i = 0;
 	int fd = open("../test.c", O_RDONLY);
-	get_next_line(fd, &line);
-	get_next_line(fd, &line);
+	while (i < 4)
+	{
+		get_next_line(fd, &line);
+		printf("%s\n", line);
+		i++;
+	}
 
 	printf("%d", fd);
 }
